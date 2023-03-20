4ee92ef53c6183338a85a850b20c0cd53bc597d9
63070028 <63070028@kmitl.ac.th>
2023-03-20 23:42:27 +0700

    add role_signup

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,138 +1,34 @@
 <template>
-    <div class="signup">
-        <div class="signup_content">
-            <h1 class="is-size-1 has-text-centered">Sign Up</h1>
-            <div class="field">
-                <label class="label is-size-4">First Name</label>
-                <div class="control">
-                    <input :class="['input', v$.firstName.$errors.length ? 'is-danger' : '']" type="text"
-                        v-model="v$.firstName.$model">
-                </div>
-                <!-- <div class="has-text-danger" v-if="v$.firstName.$error">Field is required.</div> -->
-                <div class="has-text-danger" v-for="error of v$.firstName.$errors" :key="error.$uid">
-                    <div class="error-msg">{{ error.$message }}</div>
-                </div>
-            </div>
-            <div class="field">
-                <label class="label is-size-4">Last Name</label>
-                <div class="control">
-                    <input :class="['input', v$.lastName.$errors.length ? 'is-danger' : '']" type="text"
-                        v-model="v$.lastName.$model">
-                </div>
-                <div class="has-text-danger" v-for="error of v$.lastName.$errors" :key="error.$uid">
-                    <div class="error-msg">{{ error.$message }}</div>
-                </div>
-            </div>
-
-            <div class="field">
-                <label class="label is-size-4">Email</label>
-                <div class="control">
-                    <input :class="['input', v$.email.$errors.length ? 'is-danger' : '']" type="email"
-                        v-model="v$.email.$model">
-                </div>
-                <div class="has-text-danger" v-for="error of v$.email.$errors" :key="error.$uid">
-                    <div class="error-msg">{{ error.$message }}</div>
-                </div>
-            </div>
-
-            <div class="field">
-                <label class="label is-size-4">Password</label>
-                <div class="control">
-                    <input :class="['input', v$.password.$errors.length ? 'is-danger' : '']" type="password"
-                        v-model="v$.password.$model">
-                </div>
-                <div class="has-text-danger" v-for="error of v$.password.$errors" :key="error.$uid">
-                    <div class="error-msg">{{ error.$message }}</div>
-                </div>
-            </div>
-
-            <div class="field">
-                <label class="label is-size-4">Confirm Password</label>
-                <div class="control">
-                    <input :class="['input', v$.confirmPassword.$errors.length ? 'is-danger' : '']" type="password"
-                        v-model="v$.confirmPassword.$model">
-                </div>
-                <div class="has-text-danger" v-for="error of v$.confirmPassword.$errors" :key="error.$uid">
-                    <div class="error-msg">{{ error.$message }}</div>
-                </div>
-            </div>
-
-            <div class="field">
-                <div class="control">
-                    <button class="button is-success is-large is-fullwidth" @click="submitForm">Submit</button>
-                </div>
-            </div>
+    <div class="columns select-role">
+        <div class="column has-background-light p-1 role" @click="() => { select_role = 'applicant'; }">
+            <p class="has-text-centered p-5">หางาน</p>
+        </div>
+        <div class="column has-background-light p-1" @click="() => { select_role = 'company'; }">
+            <p class="has-text-centered p-5">หาคน</p>
         </div>
     </div>
+        <applicantSignupVue v-if="select_role === 'applicant'"></applicantSignupVue>
+        <companySignupVue v-if="select_role === 'company'"></companySignupVue>
 </template>
 
 
 <script>
-import { useVuelidate } from '@vuelidate/core'
-import { required, minLength, sameAs, helpers } from '@vuelidate/validators'
-// import axios from 'axios';
-
-const alpha = helpers.regex(/^[a-zA-Z]*$/)
-const emailValid = helpers.regex(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
-
+import applicantSignupVue from '@/components/applicant-signup.vue'
+import companySignupVue from '@/components/company-signup.vue'
 export default {
     name: "SignUp",
-    setup() {
-        return {
-            v$: useVuelidate()
-        }
+    components: {
+        applicantSignupVue,
+        companySignupVue
     },
     data() {
         return {
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: "",
-            confirmPassword: "",
+            select_role: "applicant",
         }
     },
-    validations(){
-        return {
-            firstName: {
-                required,
-                alpha: helpers.withMessage('This is alpha', alpha)
-            },
-            lastName: {
-                required,
-                alpha: helpers.withMessage('This is alpha', alpha)
-            },
-            email: {
-                required,
-                emailValid: helpers.withMessage('This email', emailValid)
-            },
-            password: {
-                required,
-                minLength: minLength(8)
-            },
-            confirmPassword: {
-                required,
-                sameAsPassword: sameAs(this.password)
-            },
-        }
-    },
+
     methods: {
-        async submitForm() {
-        //     const isFormCorrect = await this.v$.$validate()
-        //     if (!isFormCorrect) return
-        //     // let data = {
-        //     //     firstName: this.firstName,
-        //     //     lastName: this.lastName,
-        //     //     email: this.email,
-        //     //     password: this.password,
-        //     // }
-        //     // console.log(data)
 
-        //     const res = await axios.post('http://localhost:3003/signup', data, {
-        //         headers: { "Access-Control-Allow-Origin": "*" }
-        //     });
-
-        //     console.log(res)
-        }
     }
 
 
